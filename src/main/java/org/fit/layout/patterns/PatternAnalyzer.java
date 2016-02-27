1d0c0f8f0972db5c6b80b0a3bd5f6b9ea3a81c2e
Radek Burget <burgetr@fit.vutbr.cz>
2016-02-27 14:58:20 +0100

    Initial commit

---
+++ src/main/java/org/fit/layout/patterns/PatternAnalyzer.java
@@ -0,0 +1,15 @@
+/**
+ * PatternAnalyzer.java
+ *
+ * Created on 27. 2. 2016, 13:31:26 by burgetr
+ */
+package org.fit.layout.patterns;
+
+/**
+ * 
+ * @author burgetr
+ */
+public class PatternAnalyzer
+{
+
+}
