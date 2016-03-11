c043ac49b122ddf2f23503f304193511a7a3d8b8
Radek Burget <burgetr@fit.vutbr.cz>
2016-03-11 18:15:14 +0100

    Below relation fixed

--- src/main/java/org/fit/layout/patterns/RelationBelow.java
+++ src/main/java/org/fit/layout/patterns/RelationBelow.java
@@ -24,21 +24,16 @@ public class RelationBelow extends Relation
     @Override
     public float isInRelationship(Area a1, Area a2)
     {
-        //here a1 is the bootom area, a2 is the top area
+        //here a1 is the bottom area, a2 is the top area
         //we say that a1 is below a2
-        if (a1.getParentArea() != null && a1.getParentArea() == a2.getParentArea())
+        final Rectangular gp1 = getTopology().getPosition(a1);
+        final Rectangular gp2 = getTopology().getPosition(a2);
+        if (gp1.getX1() == gp2.getX1())
         {
-            final Rectangular gp1 = getTopology().getPosition(a1);
-            final Rectangular gp2 = getTopology().getPosition(a2);
-            if (gp1.getX1() == gp2.getX1())
-            {
-                float dist = a1.getBounds().getY1() - a2.getBounds().getY2();
-                float em = Math.max(a2.getFontSize(), a1.getFontSize());
-                if (dist >= 0.0f && dist < 0.8*em)
-                    return 1.0f;
-                else
-                    return 0.0f;
-            }
+            float dist = a1.getBounds().getY1() - a2.getBounds().getY2();
+            float em = Math.max(a2.getFontSize(), a1.getFontSize());
+            if (dist >= 0.0f && dist < 0.8*em)
+                return 1.0f;
             else
                 return 0.0f;
         }
