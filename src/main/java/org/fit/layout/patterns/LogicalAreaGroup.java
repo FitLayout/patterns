58dd40da09255d631f8c1e5e88101b3019ae7bb0
Radek Burget <burgetr@fit.vutbr.cz>
2016-03-17 15:00:55 +0100

    Logical area group

---
+++ src/main/java/org/fit/layout/patterns/LogicalAreaGroup.java
@@ -0,0 +1,57 @@
+/**
+ * LogicalAreaGroup.java
+ *
+ * Created on 17. 3. 2016, 14:44:58 by burgetr
+ */
+package org.fit.layout.patterns;
+
+import org.fit.layout.impl.DefaultArea;
+import org.fit.layout.model.Area;
+import org.fit.layout.model.LogicalArea;
+import org.fit.layout.model.Rectangular;
+
+/**
+ * An area that is actually created by a group of areas represented
+ * by a logical area. 
+ * @author burgetr
+ */
+public class LogicalAreaGroup extends DefaultArea
+{
+    private LogicalArea srcArea;
+    
+    
+    public LogicalAreaGroup(LogicalArea src)
+    {
+        super(computeTotalBounds(src));
+        srcArea = src;
+        addTag(srcArea.getMainTag(), 1.0f);
+    }
+    
+    @Override
+    public String getText()
+    {
+        return srcArea.getText();
+    }
+
+    @Override
+    public String getText(String separator)
+    {
+        return srcArea.getText();
+    }
+
+    //=====================================================================================
+
+    private static Rectangular computeTotalBounds(LogicalArea srcArea)
+    {
+        Rectangular ret = null;
+        for (Area a : srcArea.getAreas())
+        {
+            if (ret == null)
+                ret = new Rectangular(a.getBounds());
+            else
+                ret.expandToEnclose(a.getBounds());
+        }
+        return (ret == null) ? new Rectangular() : ret;
+    }
+    
+}
