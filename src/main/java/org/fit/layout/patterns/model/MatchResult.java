8b12946f878f8d5a4afcf5db65ea18e647e87e73
Radek Burget <burgetr@fit.vutbr.cz>
2018-05-25 13:15:37 +0200

    Avoid using minMetric twice in the result comparison

--- src/main/java/org/fit/layout/patterns/model/MatchResult.java
+++ src/main/java/org/fit/layout/patterns/model/MatchResult.java
@@ -50,7 +50,7 @@ public class MatchResult implements Comparable<MatchResult>
                     return 0; //cannot compare, use next comparators
             }
         });
-        cclist.add(new Comparator<MatchResult>() //minimal metric standard deviation value (lower is better)
+        /*cclist.add(new Comparator<MatchResult>() //minimal metric standard deviation value (lower is better)
         {
             @Override
             public int compare(MatchResult o1, MatchResult o2)
@@ -62,7 +62,7 @@ public class MatchResult implements Comparable<MatchResult>
                 else
                     return 0;
             }
-        });
+        });*/
         /*cclist.add(new Comparator<MatchResult>() //connection weight standard deviation (lower is better)
         {
             @Override
