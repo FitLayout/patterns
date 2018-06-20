f73ca439d2048180facd2eed0e6be177c8d9f7fe
Radek Burget <burgetr@fit.vutbr.cz>
2018-06-20 12:14:24 +0200

    Refactoring: separate PatternGenerator

--- src/main/java/org/fit/layout/patterns/AttributeGroupMatcher.java
+++ src/main/java/org/fit/layout/patterns/AttributeGroupMatcher.java
@@ -6,7 +6,6 @@
 package org.fit.layout.patterns;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
@@ -21,16 +20,13 @@ import org.fit.layout.model.Tag;
 import org.fit.layout.patterns.graph.Group;
 import org.fit.layout.patterns.model.AreaConnection;
 import org.fit.layout.patterns.model.AreaStyle;
-import org.fit.layout.patterns.model.ConnectionList;
 import org.fit.layout.patterns.model.ConnectionPattern;
 import org.fit.layout.patterns.model.Match;
 import org.fit.layout.patterns.model.MatchResult;
 import org.fit.layout.patterns.model.MatchStatistics;
 import org.fit.layout.patterns.model.MatcherConfiguration;
 import org.fit.layout.patterns.model.TagConnection;
-import org.fit.layout.patterns.model.TagConnectionList;
 import org.fit.layout.patterns.model.TagPair;
-import org.fit.layout.patterns.model.TagPattern;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -65,6 +61,7 @@ public class AttributeGroupMatcher extends BaseMatcher
     //areas and statistics used for configuration
     private List<Area> areas;
     private StyleGenerator styleGenerator;
+    private PatternGenerator patternGenerator;
     private RelationAnalyzer pa;
     
     //list of best configurations obtained by configure()
@@ -309,6 +306,16 @@ public class AttributeGroupMatcher extends BaseMatcher
         return areas;
     }
     
+    public Set<Tag> getTagBlacklist()
+    {
+        return tagBlacklist;
+    }
+
+    public Set<TagPair> getPairBlacklist()
+    {
+        return pairBlacklist;
+    }
+
     /**
      * Sets the configuration used for testing. When set, the configuration lookup will be limited
      * to the given configuration only, other configurations will be skipped.
@@ -495,7 +502,7 @@ public class AttributeGroupMatcher extends BaseMatcher
     {
         List<MatcherConfiguration> ret = new ArrayList<>();
         List<Map<Tag, AreaStyle>> styleMaps = styleGenerator.generateStyleMaps(MIN_SUPPORT_STYLE);
-        Set<ConnectionPattern> patterns = generateConnectionPatterns(MIN_SUPPORT_RELATIONS);
+        Set<ConnectionPattern> patterns = patternGenerator.generateConnectionPatterns(MIN_SUPPORT_RELATIONS);
         for (Map<Tag, AreaStyle> styles : styleMaps) //for all style maps
         {
             for (ConnectionPattern conns : patterns)
@@ -505,213 +512,6 @@ public class AttributeGroupMatcher extends BaseMatcher
             }
         }
         return ret;
-    }
-    
-    /**
-     * Generates all supported connection patterns for the given minimal frequency of tag instances.
-     * @param minFrequency the minimal frequency of tag connections required to consider the tag connection
-     * @return A set of generated connection patterns.
-     */
-    private Set<ConnectionPattern> generateConnectionPatterns(float minFrequency)
-    {
-        TagConnectionList all = pa.getTagConnections();
-        Set<Tag> allInclDeps = getTagsWithDependencies();
-        
-        Set<TagPattern> patterns = findConnectedTagPatterns(all, usedTags, allInclDeps);
-        log.debug("Attribute patterns: {}", patterns.size());
-        
-        Set<ConnectionPattern> ret = new HashSet<>();
-        int total = 0;
-        for (TagPattern pattern : patterns)
-        {
-            log.debug("P: " + pattern);
-            
-            /*String s = pattern.toString();
-            if (s.contains("<session, title>") && s.contains("<persons, title>") && s.contains("<title, pages>"))
-                System.out.println("jo!");*/
-            
-            List<ConnectionPattern> mappings = findMappings(pattern, all, minFrequency);
-            ret.addAll(mappings);
-            total += mappings.size();
-        }
-        log.debug("Connection patterns: found {}, unique {}", total, ret.size());
-        
-        return ret;
-    }
-    
-    /**
-     * Finds all the connection patterns that match a given tag pattern based on the existing connection list.
-     * This generates all the supported combinations of relations between the tag pairs.
-     * @param pattern The tag pattern to be used
-     * @param allConnections List of all connections to be used
-     * @param minFrequency Minimal frequency of the connection that should be considered
-     * @return A list of all the connection patterns that correspond to the tag pattern
-     */
-    private List<ConnectionPattern> findMappings(TagPattern pattern, TagConnectionList allConnections, float minFrequency)
-    {
-        List<ConnectionPattern> ret = new ArrayList<>();
-        //find candidates for every pair
-        List<List<TagConnection>> lists = new ArrayList<>(pattern.size());
-        int total = 1; //total combinations expected
-        for (TagPair pair : pattern)
-        {
-            TagConnection dep = getDependencyTagConnection(pair);
-            if (dep != null) //tag connection was already resolved in dependencies
-            {
-                List<TagConnection> depList = new ArrayList<>(1);
-                depList.add(dep);
-                lists.add(depList);
-                log.debug("    for {}-{}", pair.getO1(), pair.getO2());
-                log.debug("      used dependency {}", depList);
-            }
-            else //not resolved
-            {
-                TagConnection rdep = getDependencyTagConnection(pair.reverse());
-                if (rdep != null) //reverse connection was resolved; do not resolve this pair at all
-                {
-                    List<TagConnection> depList = Collections.emptyList();
-                    lists.add(depList);
-                    total = 0;
-                    log.debug("    for {}-{}", pair.getO1(), pair.getO2());
-                    log.debug("      found reverse dependency, using {}", depList);
-                }
-                else //not resolved yet, try the most supported connections found in the page
-                {
-                    ConnectionList<Tag, TagConnection> cands = allConnections.filterForPair(pair);
-                    PatternCounter<TagConnection> cnt = new PatternCounter<>(cands, 1.0f);
-                    List<TagConnection> frequent = cnt.getFrequent(minFrequency); 
-                    lists.add(frequent);
-                    total = total * frequent.size();
-                    log.debug("    for {}-{} : {}", pair.getO1(), pair.getO2(), cnt);
-                    log.debug("      used {}", frequent);
-                }
-            }
-        }
-        //iterate over all combinarions of candidates
-        if (total > 0)
-        {
-            int[] indices = new int[lists.size()];
-            Arrays.fill(indices, 0);
-            final int lastcnt = lists.get(lists.size() - 1).size();
-            while (indices[indices.length-1] < lastcnt)
-            {
-                ConnectionPattern newItem = new ConnectionPattern(indices.length);
-                for (int i = 0; i < indices.length; i++)
-                    newItem.add(lists.get(i).get(indices[i]));
-                ret.add(newItem);
-                
-                //increment the indices
-                indices[0]++;
-                for (int i = 0; i < indices.length - 1; i++)
-                {
-                    if (indices[i] >= lists.get(i).size())
-                    {
-                        indices[i] = 0;
-                        indices[i+1]++;
-                    }
-                }
-            }
-        }
-        log.debug("    {} combinarions used", ret.size());
-        return ret;
-    }
-    
-    //===========================================================================================
-    
-    /**
-     * Discovers all usable tag patterns from the list of connections.
-     * @param attlist the list of attributes to consider
-     * @param allConnections the connections to consider
-     * @return a list of tag patterns
-     */
-    private Set<TagPattern> findConnectedTagPatterns(TagConnectionList allConnections, Set<Tag> localTags, Set<Tag> allowedTags)
-    {
-        //find the pairs that are not blacklisted
-        Set<TagPattern> ret = new HashSet<>();
-        for (Tag tag : allowedTags)
-        {
-            //select distinct pairs for the tag
-            Set<TagPair> pairs = findDistinctPairsForStartTag(tag, allConnections);
-            //recursively scan other connections
-            for (TagPair pair : pairs)
-            {
-                if ((localTags.contains(pair.getO1()) || localTags.contains(pair.getO2())) //one of the tags must be local
-                        && allowedTags.contains(pair.getO1()) && allowedTags.contains(pair.getO2())) //all must be allowed
-                {
-                    if (pairBlacklist.contains(pair))
-                    {
-                        log.debug("Blacklisted (M:N): {}", pair);
-                    }
-                    else if (tagBlacklist.contains(pair.getO1()))
-                    {
-                        log.debug("Blacklisted (M:1): {}", pair);
-                    }
-                    else
-                    {
-                        TagPattern seed = new TagPattern(usedTags.size() - 1);
-                        seed.add(pair);
-                        if (localTags.size() <= 2)
-                            ret.add(seed); //only two tags, no further search necessary
-                        else
-                            recursiveAddConnected(seed, allConnections, localTags, ret);
-                    }
-                }
-            }
-        }
-        return ret;
-    }
-    
-    /**
-     * Takes a tag pattern and recursively scans for all pairs that may be added based on the existing connection list.
-     * When the pattern is complete, it is stored to a destination pattern collection. 
-     * @param current current (incomplete) tag pattern
-     * @param attlist list of attributes to consider
-     * @param allConnections list of tag connections to consider
-     * @param dest the destination pattern collection
-     */
-    private void recursiveAddConnected(TagPattern current, TagConnectionList allConnections, Set<Tag> localTags, Collection<TagPattern> dest)
-    {
-        //try to connect all the tags already covered by the tag pattern
-        for (Tag tag : current.getTags())
-        {
-            Set<TagPair> pairs = findDistinctPairsForStartTag(tag, allConnections);
-            for (TagPair pair : pairs)
-            {
-                if (localTags.contains(pair.getO1())) //exclude unused tags
-                {
-                    if (!tagBlacklist.contains(pair.getO1()) && !pairBlacklist.contains(pair) && current.mayAdd(pair)) //a new pair may be added
-                    {
-                        TagPattern next = new TagPattern(current);
-                        next.add(pair);
-                        if (next.size() >= localTags.size() - 1) //the pattern is complete, store it
-                        {
-                            dest.add(next);
-                        }
-                        else //incomplete pair, continue the search recursively
-                        {
-                            recursiveAddConnected(next, allConnections, localTags, dest);
-                        }
-                    }
-                }
-            }
-        }
-    }
-    
-    /**
-     * Finds all distinct pairs of tags in a connection list with the given source tag.
-     * @param sourceTag the source tag (the second tag in the connection)
-     * @param allConnections the list of tag connections to use
-     * @return Resulting set of pairs.
-     */
-    private Set<TagPair> findDistinctPairsForStartTag(Tag sourceTag, TagConnectionList allConnections)
-    {
-        Set<TagPair> pairs = new HashSet<>();
-        for (TagConnection cand : allConnections.filterForSecondNode(sourceTag))
-        {
-            if (!cand.getA1().equals(cand.getA2())) //exclude reflexive pairs
-                pairs.add(cand.toPair());
-        }
-        return pairs;
     }
     
     //===========================================================================================
@@ -1112,9 +912,14 @@ public class AttributeGroupMatcher extends BaseMatcher
     {
         //create style generator
         styleGenerator = new StyleGenerator(attrs, areas, getUseStyleWildcards());
+        
         //create pattern analyzer
         //pa = new RelationAnalyzer(areas);
         pa = new RelationAnalyzerSymmetric(areas);
+        
+        //create pattern generator
+        patternGenerator = new PatternGenerator(this);
+        
         //discover tag chains used for disambiguation
         /*ConsistentAreaAnalyzer ca = new ConsistentAreaAnalyzer(pa, getTags(), attrs.get(0).getMinSupport());
         chains = ca.findConsistentChains(new RelationUnder());
---
+++ src/main/java/org/fit/layout/patterns/PatternGenerator.java
@@ -0,0 +1,248 @@
+/**
+ * PatternGenerator.java
+ *
+ * Created on 20. 6. 2018, 11:02:14 by burgetr
+ */
+package org.fit.layout.patterns;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.fit.layout.model.Tag;
+import org.fit.layout.patterns.model.ConnectionList;
+import org.fit.layout.patterns.model.ConnectionPattern;
+import org.fit.layout.patterns.model.TagConnection;
+import org.fit.layout.patterns.model.TagConnectionList;
+import org.fit.layout.patterns.model.TagPair;
+import org.fit.layout.patterns.model.TagPattern;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 
+ * @author burgetr
+ */
+public class PatternGenerator
+{
+    private static Logger log = LoggerFactory.getLogger(PatternGenerator.class);
+
+    private AttributeGroupMatcher matcher;
+    
+    
+    public PatternGenerator(AttributeGroupMatcher matcher)
+    {
+        this.matcher = matcher;
+    }
+
+    //===========================================================================================
+
+    /**
+     * Generates all supported connection patterns for the given minimal frequency of tag instances.
+     * @param minFrequency the minimal frequency of tag connections required to consider the tag connection
+     * @return A set of generated connection patterns.
+     */
+    public Set<ConnectionPattern> generateConnectionPatterns(float minFrequency)
+    {
+        TagConnectionList all = matcher.getRelationAnalyzer().getTagConnections();
+        
+        Set<TagPattern> patterns = findConnectedTagPatterns(all, matcher.getUsedTags(), matcher.getTagsWithDependencies());
+        log.debug("Attribute patterns: {}", patterns.size());
+        
+        Set<ConnectionPattern> ret = new HashSet<>();
+        int total = 0;
+        for (TagPattern pattern : patterns)
+        {
+            log.debug("P: " + pattern);
+            
+            /*String s = pattern.toString();
+            if (s.contains("<session, title>") && s.contains("<persons, title>") && s.contains("<title, pages>"))
+                System.out.println("jo!");*/
+            
+            List<ConnectionPattern> mappings = findMappings(pattern, all, minFrequency);
+            ret.addAll(mappings);
+            total += mappings.size();
+        }
+        log.debug("Connection patterns: found {}, unique {}", total, ret.size());
+        
+        return ret;
+    }
+    
+    /**
+     * Discovers all usable tag patterns from the list of connections.
+     * @param attlist the list of attributes to consider
+     * @param allConnections the connections to consider
+     * @return a list of tag patterns
+     */
+    private Set<TagPattern> findConnectedTagPatterns(TagConnectionList allConnections, Set<Tag> localTags, Set<Tag> allowedTags)
+    {
+        //find the pairs that are not blacklisted
+        Set<TagPattern> ret = new HashSet<>();
+        for (Tag tag : allowedTags)
+        {
+            //select distinct pairs for the tag
+            Set<TagPair> pairs = findDistinctPairsForStartTag(tag, allConnections);
+            //recursively scan other connections
+            for (TagPair pair : pairs)
+            {
+                if ((localTags.contains(pair.getO1()) || localTags.contains(pair.getO2())) //one of the tags must be local
+                        && allowedTags.contains(pair.getO1()) && allowedTags.contains(pair.getO2())) //all must be allowed
+                {
+                    if (matcher.getPairBlacklist().contains(pair))
+                    {
+                        log.debug("Blacklisted (M:N): {}", pair);
+                    }
+                    else if (matcher.getTagBlacklist().contains(pair.getO1()))
+                    {
+                        log.debug("Blacklisted (M:1): {}", pair);
+                    }
+                    else
+                    {
+                        TagPattern seed = new TagPattern(matcher.getUsedTags().size() - 1);
+                        seed.add(pair);
+                        if (localTags.size() <= 2)
+                            ret.add(seed); //only two tags, no further search necessary
+                        else
+                            recursiveAddConnected(seed, allConnections, localTags, ret);
+                    }
+                }
+            }
+        }
+        return ret;
+    }
+    
+    /**
+     * Finds all the connection patterns that match a given tag pattern based on the existing connection list.
+     * This generates all the supported combinations of relations between the tag pairs.
+     * @param pattern The tag pattern to be used
+     * @param allConnections List of all connections to be used
+     * @param minFrequency Minimal frequency of the connection that should be considered
+     * @return A list of all the connection patterns that correspond to the tag pattern
+     */
+    private List<ConnectionPattern> findMappings(TagPattern pattern, TagConnectionList allConnections, float minFrequency)
+    {
+        List<ConnectionPattern> ret = new ArrayList<>();
+        //find candidates for every pair
+        List<List<TagConnection>> lists = new ArrayList<>(pattern.size());
+        int total = 1; //total combinations expected
+        for (TagPair pair : pattern)
+        {
+            TagConnection dep = matcher.getDependencyTagConnection(pair);
+            if (dep != null) //tag connection was already resolved in dependencies
+            {
+                List<TagConnection> depList = new ArrayList<>(1);
+                depList.add(dep);
+                lists.add(depList);
+                log.debug("    for {}-{}", pair.getO1(), pair.getO2());
+                log.debug("      used dependency {}", depList);
+            }
+            else //not resolved
+            {
+                TagConnection rdep = matcher.getDependencyTagConnection(pair.reverse());
+                if (rdep != null) //reverse connection was resolved; do not resolve this pair at all
+                {
+                    List<TagConnection> depList = Collections.emptyList();
+                    lists.add(depList);
+                    total = 0;
+                    log.debug("    for {}-{}", pair.getO1(), pair.getO2());
+                    log.debug("      found reverse dependency, using {}", depList);
+                }
+                else //not resolved yet, try the most supported connections found in the page
+                {
+                    ConnectionList<Tag, TagConnection> cands = allConnections.filterForPair(pair);
+                    PatternCounter<TagConnection> cnt = new PatternCounter<>(cands, 1.0f);
+                    List<TagConnection> frequent = cnt.getFrequent(minFrequency); 
+                    lists.add(frequent);
+                    total = total * frequent.size();
+                    log.debug("    for {}-{} : {}", pair.getO1(), pair.getO2(), cnt);
+                    log.debug("      used {}", frequent);
+                }
+            }
+        }
+        //iterate over all combinarions of candidates
+        if (total > 0)
+        {
+            int[] indices = new int[lists.size()];
+            Arrays.fill(indices, 0);
+            final int lastcnt = lists.get(lists.size() - 1).size();
+            while (indices[indices.length-1] < lastcnt)
+            {
+                ConnectionPattern newItem = new ConnectionPattern(indices.length);
+                for (int i = 0; i < indices.length; i++)
+                    newItem.add(lists.get(i).get(indices[i]));
+                ret.add(newItem);
+                
+                //increment the indices
+                indices[0]++;
+                for (int i = 0; i < indices.length - 1; i++)
+                {
+                    if (indices[i] >= lists.get(i).size())
+                    {
+                        indices[i] = 0;
+                        indices[i+1]++;
+                    }
+                }
+            }
+        }
+        log.debug("    {} combinarions used", ret.size());
+        return ret;
+    }
+    
+    /**
+     * Takes a tag pattern and recursively scans for all pairs that may be added based on the existing connection list.
+     * When the pattern is complete, it is stored to a destination pattern collection. 
+     * @param current current (incomplete) tag pattern
+     * @param attlist list of attributes to consider
+     * @param allConnections list of tag connections to consider
+     * @param dest the destination pattern collection
+     */
+    private void recursiveAddConnected(TagPattern current, TagConnectionList allConnections, Set<Tag> localTags, Collection<TagPattern> dest)
+    {
+        //try to connect all the tags already covered by the tag pattern
+        for (Tag tag : current.getTags())
+        {
+            Set<TagPair> pairs = findDistinctPairsForStartTag(tag, allConnections);
+            for (TagPair pair : pairs)
+            {
+                if (localTags.contains(pair.getO1())) //exclude unused tags
+                {
+                    if (!matcher.getTagBlacklist().contains(pair.getO1()) && !matcher.getPairBlacklist().contains(pair) && current.mayAdd(pair)) //a new pair may be added
+                    {
+                        TagPattern next = new TagPattern(current);
+                        next.add(pair);
+                        if (next.size() >= localTags.size() - 1) //the pattern is complete, store it
+                        {
+                            dest.add(next);
+                        }
+                        else //incomplete pair, continue the search recursively
+                        {
+                            recursiveAddConnected(next, allConnections, localTags, dest);
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    /**
+     * Finds all distinct pairs of tags in a connection list with the given source tag.
+     * @param sourceTag the source tag (the second tag in the connection)
+     * @param allConnections the list of tag connections to use
+     * @return Resulting set of pairs.
+     */
+    private Set<TagPair> findDistinctPairsForStartTag(Tag sourceTag, TagConnectionList allConnections)
+    {
+        Set<TagPair> pairs = new HashSet<>();
+        for (TagConnection cand : allConnections.filterForSecondNode(sourceTag))
+        {
+            if (!cand.getA1().equals(cand.getA2())) //exclude reflexive pairs
+                pairs.add(cand.toPair());
+        }
+        return pairs;
+    }
+    
+}
