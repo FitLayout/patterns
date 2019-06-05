05e1c12c0eb592bbcbc0431fba80e262657dc76b
Radek Burget <burgetr@fit.vutbr.cz>
2019-06-05 16:01:48 +0200

    Incremental connection statistics

--- src/main/java/org/fit/layout/patterns/eval/ConnectionStats.java
+++ src/main/java/org/fit/layout/patterns/eval/ConnectionStats.java
@@ -18,17 +18,11 @@ import org.fit.layout.patterns.model.TagConnection;
  * 
  * @author burgetr
  */
-public class ConnectionStats extends HashSet<Set<AreaConnection>>
+public class ConnectionStats
 {
-    private static final long serialVersionUID = 1L;
-    
     private TagConnection tcon;
-    private Map<Metric, Float> metricMin;
-    private Map<Metric, Float> metricMax;
-    private Map<Metric, Float> metricAvg;
-    private Map<Metric, Float> metricSigma;
-    private Metric bestMetric;
-    private float bestMetricValue;
+    private int count;
+    private Map<Metric, Values> metricValues;
 
     /**
      * Creates tag statistics for a given tag connection.
@@ -37,6 +31,8 @@ public class ConnectionStats extends HashSet<Set<AreaConnection>>
     public ConnectionStats(TagConnection tcon)
     {
         this.tcon = tcon;
+        count = 0;
+        metricValues = new HashMap<>();
     }
 
     /**
@@ -55,10 +51,8 @@ public class ConnectionStats extends HashSet<Set<AreaConnection>>
      */
     public float getMetricMin(Metric m)
     {
-        if (metricMin == null)
-            evaluateMetrics();
-        Float ret = metricMin.get(m);
-        return ret == null ? -1.0f : ret;
+        final Values vals = metricValues.get(m);
+        return vals == null ? -1.0f : vals.min;
     }
     
     /**
@@ -68,10 +62,8 @@ public class ConnectionStats extends HashSet<Set<AreaConnection>>
      */
     public float getMetricMax(Metric m)
     {
-        if (metricMax == null)
-            evaluateMetrics();
-        Float ret = metricMax.get(m);
-        return ret == null ? -1.0f : ret;
+        final Values vals = metricValues.get(m);
+        return vals == null ? -1.0f : vals.max;
     }
     
     /**
@@ -81,10 +73,8 @@ public class ConnectionStats extends HashSet<Set<AreaConnection>>
      */
     public float getMetricAvg(Metric m)
     {
-        if (metricAvg == null)
-            evaluateMetrics();
-        Float ret = metricAvg.get(m);
-        return ret == null ? -1.0f : ret;
+        final Values vals = metricValues.get(m);
+        return vals == null ? -1.0f : (vals.sum / count);
     }
     
     /**
@@ -94,10 +84,8 @@ public class ConnectionStats extends HashSet<Set<AreaConnection>>
      */
     public float getMetricSigma(Metric m)
     {
-        if (metricSigma == null)
-            evaluateMetrics();
-        Float ret = metricSigma.get(m);
-        return ret == null ? -1.0f : ret;
+        final Values vals = metricValues.get(m);
+        return (float) (vals == null ? -1.0f : Math.sqrt(vals.sum_sq / count));
     }
 
     /**
@@ -107,9 +95,18 @@ public class ConnectionStats extends HashSet<Set<AreaConnection>>
      */
     public Metric getBestMetric()
     {
-        if (bestMetric == null)
-            evaluateMetrics();
-        return bestMetric;
+        float min = Float.MAX_VALUE;
+        Metric best = null;
+        for (Metric m : getMetrics())
+        {
+            final float val = getMetricSigma(m);
+            if (val >= 0 && val < min)
+            {
+                min = val;
+                best = m;
+            }
+        }
+        return best;
     }
     
     /**
@@ -118,85 +115,37 @@ public class ConnectionStats extends HashSet<Set<AreaConnection>>
      */
     public float getBestMetricValue()
     {
-        if (bestMetric == null)
-            evaluateMetrics();
-        return bestMetricValue;
+        float min = Float.MAX_VALUE;
+        for (Metric m : getMetrics())
+        {
+            final float val = getMetricSigma(m);
+            if (val >= 0 && val < min)
+            {
+                min = val;
+            }
+        }
+        return min;
     }
     
     //==================================================================================================
-    
-    /**
-     * Computes the min, max, avg and sigma values for all the metrics.
-     */
-    private void evaluateMetrics()
+
+    public void add(Set<AreaConnection> cons)
     {
-        metricMin = new HashMap<>();
-        metricMax = new HashMap<>();
-        metricAvg = new HashMap<>();
-        metricSigma = new HashMap<>();
-        
-        if (size() > 0)
+        count++;
+        for (Metric m : getMetrics())
         {
-            for (Metric m : getMetrics())
+            final float val = m.compute(cons);
+            Values vals = metricValues.get(m);
+            if (vals == null)
             {
-                float[] values = computeMetricValues(m);
-                //compute statistics
-                float min, max, sum;
-                min = max = sum = values[0];
-                for (int i = 1; i < values.length; i++)
-                {
-                    if (values[i] < min)
-                        min = values[i];
-                    if (values[i] > max)
-                        max = values[i];
-                    sum += values[i];
-                }
-                float avg = sum / values.length;
-                //sigma
-                float difsum = 0;
-                for (int i = 0; i < values.length; i++)
-                {
-                    float dif = values[i] - avg;
-                    difsum += dif * dif;
-                }
-                float sigma = (float) Math.sqrt(difsum / values.length);
-                //save the results
-                metricMin.put(m, AreaUtils.statRound(min));
-                metricMax.put(m, AreaUtils.statRound(max));
-                metricAvg.put(m, AreaUtils.statRound(avg));
-                metricSigma.put(m, AreaUtils.statRound(sigma));
+                vals = new Values(val);
+                metricValues.put(m, vals);
             }
-            //find the metric the best value
-            bestMetricValue = 0;
-            bestMetric = null;
-            for (Metric m : getMetrics())
+            else
             {
-                float val = metricSigma.get(m);
-                if (bestMetric == null || val < bestMetricValue)
-                {
-                    bestMetric = m;
-                    bestMetricValue = val;
-                }
+                vals.add(val, count);
             }
-        }
-        
-    }
-    
-    /**
-     * Computes the values of the given metrics for all the area connections represented
-     * by this connection statistics object.
-     * @param m the metric to be computed
-     * @return The array containing the values of the given metric for the indidividual
-     * area connections. The length of the array corresponds to the size of the
-     * connection stats set.
-     */
-    private float[] computeMetricValues(Metric m)
-    {
-        float[] ret = new float[this.size()];
-        int i = 0;
-        for (Set<AreaConnection> con : this)
-            ret[i++] = m.compute(con);
-        return ret;
+        }        
     }
     
     //=============================================================================================
@@ -206,7 +155,27 @@ public class ConnectionStats extends HashSet<Set<AreaConnection>>
         public float min;
         public float max;
         public float sum;
+        public float sum_sq;
+        
+        public Values(float val)
+        {
+            min = max = sum = val;
+            sum_sq = 0;
+        }
+        
+        public void add(float val, int newcount)
+        {
+            if (val < min)
+                min = val;
+            if (val > max)
+                max = val;
+            
+            final float d1 = val - (sum / (newcount - 1));
+            sum += val;
+            final float d2 = val - (sum / newcount);
+            sum_sq = sum_sq + d1 * d2;
+        }
+        
     }
-    
     
 }
